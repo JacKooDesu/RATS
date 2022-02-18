e8af1b632e4f89db6d85bb3c445be0a70171b45c
DESKTOP-0TBJVPI\JacKoo <kuo741258@gmail.com>
2022-02-18 20:26:57 +0800

    esc panel

---
+++ Assets/Scripts/EscPanel.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscPanel : MonoBehaviour
+{
+    public Button continueBtn, mainMenuBtn, leaveBtn;
+
+    private void OnEnable()
+    {
+        mainMenuBtn.onClick.RemoveAllListeners();
+        leaveBtn.onClick.RemoveAllListeners();
+
+        // mainMenuBtn.onClick.AddListener()
+        leaveBtn.onClick.AddListener(() => { print("QUIT"); Application.Quit(); });
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Animator ani = GetComponentInChildren<Animator>();
+        ani.SetTrigger("Open");
+
+        StartCoroutine(Pausing());
+    }
+
+    IEnumerator Pausing()
+    {
+        bool breaker = false;
+        UnityEngine.UI.Button b = continueBtn;
+        UnityEngine.Events.UnityAction action = null;
+        action = () =>
+        {
+            breaker = true;
+        };
+        b.onClick.AddListener(action);
+
+        while (!breaker)
+        {
+            yield return null;
+        }
+
+        Time.timeScale = 1;
+        GetComponentInChildren<Animator>().SetTrigger("Close");
+        b.onClick.RemoveAllListeners();
+    }
+}
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,37 +4,7 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
-    public GameObject pausePanel;
-
-    public void Pause()
-    {
-        Time.timeScale = 0;
-        Animator ani = pausePanel.GetComponentInChildren<Animator>();
-        ani.SetTrigger("Open");
-
-        StartCoroutine(Pausing());
-    }
-
-    IEnumerator Pausing()
-    {
-        bool breaker = false;
-        UnityEngine.UI.Button b = pausePanel.transform.GetChild(0).Find("Continue").GetComponent<UnityEngine.UI.Button>();
-        UnityEngine.Events.UnityAction action = null;
-        action = () =>
-        {
-            breaker = true;
-        };
-        b.onClick.AddListener(action);
-
-        while (!breaker)
-        {
-            yield return null;
-        }
-
-        Time.timeScale = 1;
-        pausePanel.GetComponentInChildren<Animator>().SetTrigger("Close");
-        b.onClick.RemoveAllListeners();
-    }
+    public EscPanel pausePanel;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +16,6 @@ public class GameHandler : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale > 0)
-            Pause();
+            pausePanel.Pause();
     }
 }
